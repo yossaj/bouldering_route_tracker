2254295d655f79df13247beb96b9d1701f006c12
yossaj <yossaj@icloud.com>
2020-01-12 21:41:17 +0000

    refactoring database with Dao and Room arch components

---
+++ app/src/main/java/com/example/boulderjournal/data/RouteDao.java
@@ -0,0 +1,29 @@
+package com.example.boulderjournal.data;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface RouteDao {
+
+    @Query("SELECT * FROM route")
+    List<RouteEntry> loadAllRoutes();
+
+    @Insert
+    void insertRoute(RouteEntry routeEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateRoute(RouteEntry routeEntry);
+
+    @Delete
+    void deleteRoute(RouteEntry routeEntry);
+
+
+
+}
---
+++ app/src/main/java/com/example/boulderjournal/data/RouteEntry.java
@@ -0,0 +1,94 @@
+package com.example.boulderjournal.data;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+
+import java.util.Date;
+
+@Entity(tableName = "route")
+public class RouteEntry {
+
+    private int id;
+    private String mRouteName;
+    private String mRouteColour;
+    private String mRoom;
+    private String mWall;
+    private String mNote;
+    private Date updatedAt;
+
+
+    @Ignore
+    public RouteEntry(String routeName, String routeColour, String room, String wall, String note , Date updatedAt){
+        this.mRouteName = routeName;
+        this.mRouteColour = routeColour;
+        this.mRoom = room;
+        this.mWall = wall;
+        this.mNote = note;
+        this.updatedAt = updatedAt;
+    }
+
+    public RouteEntry(int id, String routeName, String routeColour, String room, String wall, String note , Date updatedAt){
+        this.id = id;
+        this.mRouteName = routeName;
+        this.mRouteColour = routeColour;
+        this.mRoom = room;
+        this.mWall = wall;
+        this.mNote = note;
+        this.updatedAt = updatedAt;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRouteName() {
+        return mRouteName;
+    }
+
+    public void setRouteName(String routeName) {
+        this.mRouteName = routeName;
+    }
+
+    public String getRouteColour() {
+        return mRouteColour;
+    }
+
+    public void setRouteColour(String routeColour) {
+        this.mRouteColour = routeColour;
+    }
+
+    public String getRoom(){
+        return mRoom;
+    }
+
+    public String getWall() {
+        return mWall;
+    }
+
+    public void setWall(String wall) {
+        this.mWall = wall;
+    }
+
+    public String getNote(){
+        return mNote;
+    }
+
+    public void setNote(String note){
+        this.mNote = note;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+}
