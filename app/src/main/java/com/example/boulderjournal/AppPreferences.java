302b0889091d18bb2370f690c677288063be70ad
yossaj <yossaj@icloud.com>
2020-01-28 09:54:09 +0000

    added listner for day changes

--- app/src/main/java/com/example/boulderjournal/AppPreferences.java
+++ app/src/main/java/com/example/boulderjournal/AppPreferences.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+import ca.antonious.materialdaypicker.MaterialDayPicker;
+
 public class AppPreferences extends AppCompatActivity {
+
+    private MaterialDayPicker dayPicker;
+    private String climbDay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_preferences);
+        dayPicker = (MaterialDayPicker)findViewById(R.id.day_picker);
+        setDayPickerListener();
+
+    }
+
+    public void setDayPickerListener(){
+        dayPicker.setDaySelectionChangedListener(new MaterialDayPicker.DaySelectionChangedListener() {
+            @Override
+            public void onDaySelectionChanged(List<MaterialDayPicker.Weekday> selectedDay) {
+                 climbDay = selectedDay.toString();
+            }
+        });
     }
 }
